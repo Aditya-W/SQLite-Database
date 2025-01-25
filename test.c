dccad9b31ea6d698a0779082054842f511c4baf7
Aditya-W <adityav0810@gmail.com>
2025-01-25 12:32:46 -0800

    Added meta commands

--- test.c
+++ test.c
@@ -2,30 +2,36 @@
 
 
 
+ int main(int argc, char* argv[]) {
+   InputBuffer* input_buffer = new_input_buffer();
+   while (true) {
+     print_prompt();
+     read_input(input_buffer);
 
-typedef struct
-{
-    char* buffer;
-    size_t buffer_length ; // defining the buffer length
-    ssize_t input_length; 
-} InputBuffer;
+-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
 
-int main(int argc, char* argv[]) {
-    InputBuffer* input_buffer = new_input_buffer(); // defining an input buffer to get a line of input
-    while(true) // infinite prompt loop
-    {
-        print_prompt(); // user defined print function that prints the prompt
-        read_input(input_buffer); // reads the user input from the input buffer
-
-        if (strcmp(input_buffer->buffer, ".exit") == 0)
-        {
-            close_input_buffer(input_buffer); // function to exit the loop
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
-        }
-    }
-}
-
++    if (input_buffer->buffer[0] == '.') {
++      switch (do_meta_command(input_buffer)) {
++        case (META_COMMAND_SUCCESS):
++          continue;
++        case (META_COMMAND_UNRECOGNIZED_COMMAND):
++          printf("Unrecognized command '%s'\n", input_buffer->buffer);
++          continue;
++      }
+     }
++
++    Statement statement;
++    switch (prepare_statement(input_buffer, &statement)) {
++      case (PREPARE_SUCCESS):
++        break;
++      case (PREPARE_UNRECOGNIZED_STATEMENT):
++        printf("Unrecognized keyword at start of '%s'.\n",
++               input_buffer->buffer);
++        continue;
++    }
++
++    execute_statement(&statement);
++    printf("Executed.\n");
+   }
+ }
+ 
